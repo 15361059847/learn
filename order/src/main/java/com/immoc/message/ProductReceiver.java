436f1daa5e13a2e0720ab673ff685684864ace4c
15361059847 <525504136@qq.com>
2020-06-24 10:25:39 +0800

    no message

--- order/src/main/java/com/immoc/message/ProductReceiver.java
+++ order/src/main/java/com/immoc/message/ProductReceiver.java
@@ -5,6 +5,8 @@ import com.immoc.utils.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 
@@ -12,11 +14,19 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class ProductReceiver {
 
+    private static final  String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message){
         ProductInfo productInfo =(ProductInfo) JsonUtil.fromJson(message, ProductInfo.class);
         log.info("从队列【{}】接收消息:{}" ,"productInfo",productInfo);
-        //
+        //存储到redis
+        stringRedisTemplate.opsForValue().set(
+                String.format(PRODUCT_STOCK_TEMPLATE,productInfo.getProductId()),productInfo.getProductStock().toString());
     }
 
 
